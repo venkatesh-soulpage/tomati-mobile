2e95e55a414bf60a433665a3e11cdb51fb93a327
preetham-soulpage <preetham.varanasi@soulpageit.com>
2021-03-19 16:11:04 +0530

    Sentry Tracing is included

--- src/index.js
+++ src/index.js
@@ -11,18 +11,30 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/stylesheets/main.scss";
 // sentry
 import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 // dotenv
 require("dotenv").config();
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
 });
 
 // Creating REDUX store with the required configurations
 const store = configureStore();
+console.log(
+  process.env.REACT_APP_SENTRY_DSN,
+  "process.env.REACT_APP_SENTRY_DSN"
+);
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <App />
+    </Sentry.ErrorBoundary>
   </Provider>,
   document.getElementById("root")
 );
