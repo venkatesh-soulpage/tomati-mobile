f34fa4a00299d94df735ada385c0586368d78b6d
saikrishna-soulpage <saikrishna.bandhu@soulpageit.com>
2021-04-21 14:34:31 +0530

    changes for Inactive menu

--- src/pages/Outlet/index.js
+++ src/pages/Outlet/index.js
@@ -4,7 +4,6 @@ import { connect } from "react-redux";
 import * as Action from "_actions";
 // Router
 import { withRouter, useLocation } from "react-router-dom";
-import { Button } from "react-bootstrap";
 import _ from "lodash";
 import CoverImage from "./CoverImage";
 import Level1 from "./Level1";
@@ -12,7 +11,6 @@ import Level2 from "./Level2";
 import Level3 from "./Level3";
 import Loading from "components/Loading";
 import Header from "components/Header";
-
 const Index = (props) => {
   const [state, setState] = React.useState({
     showMenu: false,
@@ -26,7 +24,6 @@ const Index = (props) => {
     return new URLSearchParams(useLocation().search);
   };
   const query = useQuery();
-
   const {
     showMenu,
     menu_category,
@@ -38,53 +35,38 @@ const Index = (props) => {
   React.useEffect(() => {
     props.dispatch(Action.getOutlet(query.get("outlet_venue")));
   }, []);
-
   const { outlet } = props.outlet;
-
   if (props?.outlet?.isFetching || !outlet) {
     return <Loading textSecondary={true} />;
   }
-
   if (props?.outlet?.error) {
     return <div>Invalid Venue</div>;
   }
-
-  if (!outlet?.is_venue_active) {
-    return <div> Menu is inactive. Please contact restaurant manager</div>;
-  }
-
   const { name, description, menu, cover_image, logo_img, location } = outlet;
-
   // Outlet Menu Category
   const outlet_menu = _.without(
     _.map(_.uniqBy(menu, "outlet_category"), "outlet_category"),
     "",
     null
   );
-
   if (!outlet_menu.length > 0 && currentLevel === "level1") {
     setState({ currentLevel: "level2" });
   }
-
   // Product Menu Category
   let product_menu = _.without(
     _.map(_.uniqBy(menu, "product_category"), "product_category"),
     "",
     null
   );
-
   if (outlet_menu.length > 0) {
     const items = _.filter(menu, { outlet_category });
-
     product_menu = _.without(
       _.map(_.uniqBy(items, "product_category"), "product_category"),
       "",
       null
     );
   }
-
   let filtered_menu = menu;
-
   // Filtering Categories
   if (product_category && outlet_category) {
     filtered_menu = _.filter(menu, { product_category, outlet_category });
@@ -93,13 +75,10 @@ const Index = (props) => {
   } else if (!product_category && outlet_category) {
     filtered_menu = _.filter(menu, { outlet_category });
   }
-
   let items_list = filtered_menu;
-
   if (menu_category) {
     items_list = _.filter(filtered_menu, { product_category, menu_category });
   }
-
   return (
     <div className="outlet-style black-background ">
       <div>
@@ -124,29 +103,37 @@ const Index = (props) => {
                 <div className="text-white mt-3">{location.name}</div>
                 <hr className="hr" />
                 <p className="description-style">{description}</p>
-
-                <Button
-                  className="btn-v"
+                <button
+                  className="btn btn-v"
                   onClick={() => {
-                    if (!filtered_menu[0].outlet_category) {
-                      setState((state) => ({
-                        ...state,
-                        showMenu: true,
-                        outlet_category: null,
-                        currentLevel: "level2",
-                      }));
-                    } else {
-                      setState((state) => ({
-                        ...state,
-                        showMenu: true,
-                        currentLevel: "level1",
-                      }));
+                    if (outlet?.is_venue_active) {
+                      if (!filtered_menu[0].outlet_category) {
+                        setState((state) => ({
+                          ...state,
+                          showMenu: true,
+                          outlet_category: null,
+                          currentLevel: "level2",
+                        }));
+                      } else {
+                        setState((state) => ({
+                          ...state,
+                          showMenu: true,
+                          currentLevel: "level1",
+                        }));
+                      }
                     }
                   }}
-                  block
                 >
-                  View Menu
-                </Button>
+                  {outlet?.is_venue_active ? (
+                    <span>View Menu</span>
+                  ) : (
+                    <p className="m-0 lh-1">
+                      Inactive Menu
+                      <br />
+                      <small>Please contact restaurant manager</small>
+                    </p>
+                  )}
+                </button>
               </>
             ) : (
               <>
@@ -187,9 +174,7 @@ const Index = (props) => {
     </div>
   );
 };
-
 function mapStateToProps(state) {
   return { auth: state.auth, outlet: state.outlet };
 }
-
 export default withRouter(connect(mapStateToProps)(Index));
